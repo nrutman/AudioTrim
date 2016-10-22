046d225748ccc29435b66eeebe5253a37ed6f3ac
Nate Rutman <nathan.rutman@gmail.com>
2016-10-21 21:27:36 -0400

    Add gitignore

---
+++ main.js
@@ -0,0 +1,12 @@
+const {app, BrowserWindow} = require('electron');
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({width: 800, height: 600});
+    win.loadURL(`file://${__dirname}/index.html`);
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
