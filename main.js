008a8a71dae5c589ebf03bf6f668c93372dbcd4c
Nate Rutman <nathan.rutman@gmail.com>
2016-10-21 21:41:26 -0400

    Add additional main events

--- main.js
+++ main.js
@@ -9,4 +9,19 @@ function createWindow() {
     });
 }
 
+// create window when app starts
 app.on('ready', createWindow);
+
+// kill process when all windows are closed
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+// create window if app is activated (i.e. clicking on dock icon in OSX)
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
