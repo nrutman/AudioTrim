f3b89db4a90840dc91ebd572931ad1bee99460ea
Nate Rutman <nathan.rutman@gmail.com>
2016-10-23 17:43:13 -0400

    Add watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var cleanCss = require('gulp-clean-css');
 var concat = require('gulp-concat');
 var del = require('del');
 var gulp = require('gulp');
@@ -16,7 +17,9 @@ var javascriptGlob = [
     './app/ng/run/**/*.js',
     './app/ng/**/*.js'
 ];
-var stylesGlob = './app/sass/**/*.scss';
+var stylesGlob = [
+    './app/sass/**/*.scss'
+];
 
 /**
  * Tasks
@@ -39,5 +42,20 @@ gulp.task('javascript', () => {
 gulp.task('styles', () => {
     return gulp.src('./app/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCss())
         .pipe(gulp.dest('./app/dist/css'));
 });
+
+gulp.task('watch', () => {
+    var sassWatcher = gulp.watch(stylesGlob, ['styles']);
+    generateWatcherListener(sassWatcher, 'styles');
+    var javascriptWatcher = gulp.watch(javascriptGlob, ['javascript']);
+    generateWatcherListener(javascriptWatcher, 'javascript');
+});
+
+function generateWatcherListener(watcher, taskType) {
+    watcher.on('change', function(event) {
+        var displayPath = gutil.colors.yellow(event.path.replace(__dirname + '/', ''));
+        gutil.log('File', displayPath, 'was', event.type + ', recompiling ' + taskType + '...');
+    });
+}
