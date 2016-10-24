70e1446dd0093ecff5f25872bb737f367e37a587
Nate Rutman <nathan.rutman@gmail.com>
2016-10-23 23:09:34 -0400

    Add beginnings of load functionality

--- app/ng/load/LoadCtrl.js
+++ app/ng/load/LoadCtrl.js
@@ -4,6 +4,25 @@
         .module('audiotrim')
         .controller('LoadCtrl', LoadCtrl);
 
-    function LoadCtrl() {}
+    function LoadCtrl(require) {
+
+        var self = this;
+
+        var fs = require('fs');
+        var dialog = require('electron').remote.dialog;
+
+        self.showOpenDialog = showOpenDialog;
+
+        function showOpenDialog() {
+            dialog.showOpenDialog(function(fileNames) {
+                if (!fileNames) {
+                    return;
+                }
+
+                console.log(fileNames);
+            });
+        }
+
+    }
 
 })();
